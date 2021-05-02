3fe6e4c1621dcd081dacccda619ed66ecf063d2f
SaqibArsalan <saqibarsalan1997@gmail.com>
2021-05-02 13:58:31 +0500

    Factorial using for loop implementation completed

--- factorial_using_for_loop.py
+++ factorial_using_for_loop.py
@@ -1,3 +1,10 @@
 
-def factorial_using_for_loop(arr):
+def factorial_using_for_loop(num):
     fact = 1
+
+    for i in range(1, num+1):
+        fact *= i
+
+    return fact
+
+print(factorial_using_for_loop(3))
