f33a982922bca22626000433f92092403c217e9c
SaqibArsalan <saqibarsalan1997@gmail.com>
2021-05-02 14:05:13 +0500

    Error handling in Factorial using for loop

--- factorial_using_for_loop.py
+++ factorial_using_for_loop.py
@@ -2,9 +2,18 @@
 def factorial_using_for_loop(num):
     fact = 1
 
-    for i in range(1, num+1):
-        fact *= i
+    if num < 0:
+        print("Not possible")
+        return -1
+
+    elif num == 0:
+        return 1
+
+    else:
+        for i in range(1, num+1):
+            fact *= i
 
     return fact
 
-print(factorial_using_for_loop(3))
+
+print(factorial_using_for_loop(0))
